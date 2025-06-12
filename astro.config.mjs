7071f4986f4889a7472eeb1b8d94df33c0439bbb
Michael Scott <myscott1996@gmail.com>
2025-06-12 07:28:49 -0500

    Add search bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import pageFind from 'astro-pagefind';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  output: 'static',
+  site: 'https://michael-scott.info',
+  integrations: [mdx(), sitemap(), pageFind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
